53e396d398785d13a9488b6bfabd64cfe952c1bc
li2008kui <445754369@qq.com>
2015-12-03 10:11:02 +0800

    + 完善消息类型枚举

--- CSA018/MessageType.cs
+++ CSA018/MessageType.cs
@@ -1,15 +1,33 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace ThisCoder.CSA018
+﻿namespace ThisCoder.CSA018
 {
     /// <summary>
     /// 消息类型枚举
     /// </summary>
     public enum MessageType : byte
     {
+        /// <summary>
+        /// 命令请求
+        /// </summary>
+        Request = 0x01,
+
+        /// <summary>
+        /// 命令响应
+        /// </summary>
+        Response = 0x02,
+
+        /// <summary>
+        /// 事件和告警
+        /// </summary>
+        Event = 0x03,
+
+        /// <summary>
+        /// 事件和告警响应
+        /// </summary>
+        EventResp = 0x04,
+
+        /// <summary>
+        /// 命令结果
+        /// </summary>
+        Result = 0x05
     }
 }
