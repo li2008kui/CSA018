f85caef1ccfd24deaa427e08ce906055341ac48b
li2008kui <445754369@qq.com>
2015-12-08 15:07:48 +0800

    + 添加字节数组扩展方法助手类

---
+++ CSA018/ByteArrayHelper.cs
@@ -0,0 +1,31 @@
+﻿using System.Text;
+
+namespace ThisCoder.CSA018
+{
+    /// <summary>
+    /// 字节数组助手类
+    /// </summary>
+    public static class ByteArrayHelper
+    {
+        /// <summary>
+        /// 将字节数组转换为十六进制字符串
+        /// </summary>
+        /// <param name="value">要转换的字节数组</param>
+        /// <param name="separator">
+        /// 分隔符
+        ///     <para>默认为空格</para>
+        /// </param>
+        /// <returns></returns>
+        public static string ToHexString(this byte[] value, string separator = " ")
+        {
+            StringBuilder sb = new StringBuilder();
+
+            foreach (var item in value)
+            {
+                sb.Append(item.ToString("X2") + separator);
+            }
+
+            return sb.ToString().TrimEnd(separator.ToCharArray());
+        }
+    }
+}
