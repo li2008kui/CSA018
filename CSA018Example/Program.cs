342a0c7a21cc3fe0d02aa02b3c7190dd81457a1b
li2008kui <445754369@qq.com>
2015-12-08 15:14:36 +0800

    + 添加对协议调用实例的演示项目

---
+++ CSA018Example/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using ThisCoder.CSA018;
+
+namespace ThisCoder.CSA018Example
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            OperateAction oa = new OperateAction(MessageType.Request, 0x00000001);
+            byte[] cmd = oa.GetOperateCommand(MessageId.RealTimeControlLamp, ParameterType.Brightness, "70");
+
+            Console.WriteLine(cmd.ToHexString());
+            Console.ReadKey();
+        }
+    }
+}
