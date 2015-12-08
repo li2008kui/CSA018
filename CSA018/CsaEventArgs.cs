37e43ea9c8f1ee254d8353bbd1c49e4aca23d097
li2008kui <445754369@qq.com>
2015-12-08 14:02:26 +0800

    + 添加自定义事件参数类

---
+++ CSA018/CsaEventArgs.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace ThisCoder.CSA018
+{
+    /// <summary>
+    /// 自定义事件参数类。
+    /// </summary>
+    public class CsaEventArgs : EventArgs
+    {
+        /// <summary>
+        /// 消息报文对象列表。
+        /// </summary>
+        public List<Datagram> DatagramList { get; private set; }
+
+        /// <summary>
+        /// 初始化 CsaEventArgs 类的新实例。
+        /// </summary>
+        private CsaEventArgs() : base() { }
+
+        /// <summary>
+        /// 通过报文字节数组初始化 CsaEventArgs 类的新实例。
+        /// </summary>
+        /// <param name="dataArray">消息报文字节数组</param>
+        /// <param name="isTcpOrUdp">报文承载方式是否是TCP或UDP，默认为false</param>
+        /// <param name="isCheckCrc">是否校验CRC</param>
+        public CsaEventArgs(byte[] dataArray, bool isTcpOrUdp = false, bool isCheckCrc = true)
+            : base()
+        {
+            DatagramList = Datagram.GetDatagramList(dataArray, isTcpOrUdp, isCheckCrc);
+        }
+    }
+}
