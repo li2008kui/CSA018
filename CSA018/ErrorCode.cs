438a078433bb9b989ef72514019ca09050b18ea2
li2008kui <445754369@qq.com>
2015-12-11 18:20:55 +0800

    * 修正错误代码的枚举值

--- CSA018/ErrorCode.cs
+++ CSA018/ErrorCode.cs
@@ -5,59 +5,59 @@
     /// </summary>
     public enum ErrorCode : uint
     {
-        #region 系统保留(0~99和9999)
+        #region 系统保留(0000~0099和9999)
         /// <summary>
         /// 成功
         /// </summary>
-        Succeed = 0,
+        Succeed = 0x30303030,
 
         /// <summary>
         /// 消息解析错误
         /// </summary>
-        MessageParseError = 1,
+        MessageParseError = 0x30303031,
 
         /// <summary>
         /// 校验和错误
         /// </summary>
-        ChecksumError = 2,
+        ChecksumError = 0x30303032,
 
         /// <summary>
         /// 消息ID未定义
         /// </summary>
-        MessageIdUndefined = 3,
+        MessageIdUndefined = 0x30303033,
 
         /// <summary>
         /// 命令暂时不能执行
         /// </summary>
-        NotExecuted = 4,
+        NotExecuted = 0x30303034,
 
         /// <summary>
         /// 参数个数错误
         /// </summary>
-        ParameterCountError = 5,
+        ParameterCountError = 0x30303035,
 
         /// <summary>
         /// 参数格式错误
         /// </summary>
-        ParameterFormatError = 6,
+        ParameterFormatError = 0x30303036,
 
         /// <summary>
         /// 参数范围错误
         /// </summary>
-        ParameterScopeError = 7,
+        ParameterScopeError = 0x30303037,
 
         /// <summary>
         /// 参数类型未定义
         /// </summary>
-        ParameterTypeUndefined = 8,
+        ParameterTypeUndefined = 0x30303038,
 
         /// <summary>
         /// 未知错误
         /// </summary>
-        Unknown = 9999,
+        Unknown = 0x39393939,
         #endregion
 
-        #region 厂商自定义(100~9998)
+        #region 厂商自定义(0100~9998)
 
         #endregion
     }
